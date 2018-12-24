72ed403ee2291732ac5442c88eccabc4a9cc3679
Felipe <Feelipe@email.com>
2018-12-24 15:53:57 -0200

    [013] Desafio Avançado

--- js/button-store.js
+++ js/button-store.js
@@ -1,7 +1,8 @@
 const $button = document.querySelector(".-second");
+const $bag = document.querySelector(".-last");
 
 $button.addEventListener("click", handleClick);
 
 function handleClick() {
-  console.log("Fui Clicado!");
+  $bag.textContent = "Carrinho(11)";
 }
