13e5300a19783441e40032a9a36be435820d4d7a
Felipe <Feelipe@email.com>
2018-12-26 15:13:06 -0200

    [014] Desafio Intermediário

--- js/description-product.js
+++ js/description-product.js
@@ -1,7 +1,13 @@
 const $heart = document.querySelector(".-heart");
+const $stars = document.querySelector(".star");
 
-$heart.addEventListener("click", handleClick);
+$heart.addEventListener("click", handleClickHeart);
+$stars.addEventListener("click", handleClickStar);
 
-function handleClick() {
+function handleClickHeart() {
   $heart.classList.toggle("-active");
+}
+
+function handleClickStar() {
+  $stars.classList.toggle("-active");
 }
