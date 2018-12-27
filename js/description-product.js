503c6639f74aabd9e71015c64e53330d7402851a
Felipe <Feelipe@email.com>
2018-12-27 10:27:55 -0200

    [015] Desafio Unico

--- js/description-product.js
+++ js/description-product.js
@@ -3,10 +3,20 @@ const $stars = document.querySelectorAll(".star");
 
 $heart.addEventListener("click", handleClick);
 
-$stars.forEach(function($star, key) {
-  $star.addEventListener("click", handleClick);
-});
-
 function handleClick() {
   this.classList.toggle("-active");
 }
+
+$stars.forEach(function($star, itemSelected) {
+  $star.addEventListener("click", starSelected);
+
+  function starSelected() {
+    $stars.forEach(function(star, index) {
+      if (itemSelected >= index) {
+        star.classList.add("-active");
+      } else {
+        star.classList.remove("-active");
+      }
+    });
+  }
+});
