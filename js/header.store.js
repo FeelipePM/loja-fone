bf605c965d92af864e94f68a8da1fd9b5b462dd0
Felipe <Feelipe@email.com>
2019-01-03 21:06:00 -0200

    Desafio Básico & Intermediário- [017]

--- js/header.store.js
+++ js/header.store.js
@@ -1,5 +1,15 @@
 const $search = document.querySelector("[type=search]");
+const $iconSearch = document.querySelector(".iconSearch");
+const $iconClose = document.querySelector(".iconClose");
 
-$search.addEventListener("click", () => {
-  $search.classList.toggle("-active");
+$iconSearch.addEventListener("click", () => {
+  $iconSearch.classList.add("-active");
+  $iconClose.classList.add("-active");
+  $search.classList.add("-active");
+});
+
+$iconClose.addEventListener("click", () => {
+  $iconClose.classList.remove("-active");
+  $iconSearch.classList.remove("-active");
+  $search.classList.remove("-active");
 });
